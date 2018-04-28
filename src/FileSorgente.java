0b0d4f7989fee837ec248960770b7d9b2ec4449b
Manuel Lusenti <crucio92@outlook.com>
2018-04-28 15:43:35 +0200

    1st Commit

--- src/FileSorgente.java
+++ src/FileSorgente.java
@@ -1,8 +1,36 @@
 
 public class FileSorgente extends File {
-
+	
+	private String contenuto;
+	
 	public FileSorgente(String nome, int tipo, String percorsoFile) {
 		super(nome, tipo, percorsoFile);
+	}
+	
+	public FileSorgente(String nome,int tipo, String percorsoFile, String contenuto) {
+		this(nome,tipo,percorsoFile);
+		this.contenuto = contenuto;
+		
+	}
+
+	private String getContenuto() {
+		return contenuto;
+	}
+
+	private void setContenuto(String contenuto) {
+		this.contenuto = contenuto;
+	}
+	
+	public void aggiungiTesto(String testo) {
+		
+		if(testo == null) {
+			this.setContenuto(testo);
+		}
+		
+		assert this.getContenuto() != null: "Contenuto del file nullo";
+		
+		this.setContenuto(this.getContenuto() + testo);
+		
 	}
 
 }
